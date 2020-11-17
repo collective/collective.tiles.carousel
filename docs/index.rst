1cf6a6d86e7d80fd6e5bcae7d9afea500935f717
Peter Holzer <peter.holzer@agitator.com>
2020-11-17 10:35:13 +0100

    "Create addon: collective.tiles.bootstrapslider"

---
+++ docs/index.rst
@@ -0,0 +1,5 @@
+================================
+collective.tiles.bootstrapslider
+================================
+
+User documentation
---
+++ src/collective/tiles/bootstrapslider/tests/test_setup.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""Setup tests for this package."""
+from collective.tiles.bootstrapslider.testing import (
+    COLLECTIVE_TILES_BOOTSTRAPSLIDER_INTEGRATION_TESTING  # noqa: E501,
+)
+from plone import api
+from plone.app.testing import setRoles, TEST_USER_ID
+
+import unittest
+
+
+try:
+    from Products.CMFPlone.utils import get_installer
+except ImportError:
+    get_installer = None
+
+
+class TestSetup(unittest.TestCase):
+    """Test that collective.tiles.bootstrapslider is properly installed."""
+
+    layer = COLLECTIVE_TILES_BOOTSTRAPSLIDER_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer['request'])
+        else:
+            self.installer = api.portal.get_tool('portal_quickinstaller')
+
+    def test_product_installed(self):
+        """Test if collective.tiles.bootstrapslider is installed."""
+        self.assertTrue(self.installer.isProductInstalled(
+            'collective.tiles.bootstrapslider'))
+
+    def test_browserlayer(self):
+        """Test that ICollectiveTilesBootstrapsliderLayer is registered."""
+        from collective.tiles.bootstrapslider.interfaces import (
+            ICollectiveTilesBootstrapsliderLayer)
+        from plone.browserlayer import utils
+        self.assertIn(
+            ICollectiveTilesBootstrapsliderLayer,
+            utils.registered_layers())
+
+
+class TestUninstall(unittest.TestCase):
+
+    layer = COLLECTIVE_TILES_BOOTSTRAPSLIDER_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        if get_installer:
+            self.installer = get_installer(self.portal, self.layer['request'])
+        else:
+            self.installer = api.portal.get_tool('portal_quickinstaller')
+        roles_before = api.user.get_roles(TEST_USER_ID)
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.installer.uninstallProducts(['collective.tiles.bootstrapslider'])
+        setRoles(self.portal, TEST_USER_ID, roles_before)
+
+    def test_product_uninstalled(self):
+        """Test if collective.tiles.bootstrapslider is cleanly uninstalled."""
+        self.assertFalse(self.installer.isProductInstalled(
+            'collective.tiles.bootstrapslider'))
+
+    def test_browserlayer_removed(self):
+        """Test that ICollectiveTilesBootstrapsliderLayer is removed."""
+        from collective.tiles.bootstrapslider.interfaces import \
+            ICollectiveTilesBootstrapsliderLayer
+        from plone.browserlayer import utils
+        self.assertNotIn(
+            ICollectiveTilesBootstrapsliderLayer,
+            utils.registered_layers())
