fcaea2571ccc62fe0d6f87ee52a36927aa15b0bf
Peter Holzer <peter.holzer@agitator.com>
2020-11-17 11:45:36 +0100

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -49,14 +49,8 @@ setup(
         'setuptools',
         # -*- Extra requirements: -*-
         'z3c.jbot',
-        'Products.GenericSetup>=1.8.2',
-        'plone.api>=1.8.4',
+        'Products.CMFPlone',
         'plone.restapi',
-        'plone.app.dexterity',
-        'plone.app.referenceablebehavior',
-        'plone.app.relationfield',
-        'plone.app.lockingbehavior',
-        'plone.schema',
     ],
     extras_require={
         'test': [
