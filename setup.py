305bd79c56c649a3f12ebb4e86248b5f702c3313
Peter Mathis <peter.mathis@kombinat.at>
2023-09-21 16:28:24 +0200

    Back to development: 1.0.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.tiles.carousel",
-    version="1.0.2",
+    version="1.0.3.dev0",
     description="Slider for plone.app.mosaic based on Bootstrap 5",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
