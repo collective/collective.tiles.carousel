77245d2fcaaeda6a09d9faedb1af9603cdd09baf
Maurits van Rees <maurits@vanrees.org>
2026-08-12 22:08:07 +0200

    Back to development: 2.0.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.tiles.carousel",
-    version="2.0.0",
+    version="2.0.1.dev0",
     description="Slider for plone.app.mosaic based on Bootstrap 5",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
